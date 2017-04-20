4dca343e36d9deb01c22d6cd292189c8574603d2
Clement Teboul <clement.teboul@akka.digital>
2017-04-20 14:43:01 +0200

    Tuto part 6 ~GetHeroes

--- heroes/src/app/hero-detail.component.ts
+++ heroes/src/app/hero-detail.component.ts
@@ -4,17 +4,18 @@ import { Hero } from './hero';
 @Component({
   selector: 'hero-detail',
   template: `
-	<div *ngIf="hero">
+    <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div *ngIf = "hero">
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
+      <div>
         <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
+        <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
     </div>
-  `,
+  `
 })
-
 export class HeroDetailComponent {
-	@Input() hero : hero;
+  @Input() hero: Hero;
 }
